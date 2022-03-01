3fe3fa1559fa0b0951f695b26f868d05735bcb2c
vin350 <53111833+vin350@users.noreply.github.com>
2022-03-01 13:31:21 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 module.exports = {
-    purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}'],
     darkMode: 'class',
     theme: {
-        extend: {},
-    },
-    variants: {
         extend: {},
     },
     plugins: [],
