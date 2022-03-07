46403f09648e254a7c66d72c1cda45ca059e7aff
vin350 <53111833+vin350@users.noreply.github.com>
2022-03-07 03:57:56 -0300

    add NProgress

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
 import { ThemeProvider } from 'next-themes'
+import NextNProgress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider enabledSystem={true} attribute="class">
+      <NextNProgress />
       <Component {...pageProps} />
     </ThemeProvider>
   )
