c3a33c0db0bd68e0f83867c66ea0a3fed2852851
vin350 <53111833+vin350@users.noreply.github.com>
2022-03-12 20:36:45 -0300

    Update CSS

--- pages/MartinGale.js
+++ pages/MartinGale.js
@@ -100,7 +100,7 @@ export default function Home() {
           >
             <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
               <div className="inline-block py-2 min-w-full sm:px-6 lg:px-8">
-                <div className="overflow-hidden shadow-md sm:rounded-lg">
+                <div className="overflow-hidden shadow-md rounded-lg">
                   <table className="min-w-full">
                     <thead className="bg-gray-50 dark:bg-gray-700">
                       <tr>
